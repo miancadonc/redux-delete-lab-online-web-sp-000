dd9a7bd2bcce1349b0f8e29380c5a4043c1ae984
Nat McConnaughay <natmcconnaughay@gmail.com>
2021-04-27 08:17:27 -0500

    commit tree 6cc30c9ced997436ebe3b17a26b6e8a5c57f6ea5

--- src/components/Band.js
+++ src/components/Band.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 
 class Band extends Component {
 
+  handleClick = (e) => {
+    e.preventDefault()
+    this.props.deleteBand(this.props.id)
+  }
+
   render() {
+    debugger
     return(
       <div>
-        Band Component
+        <li>{this.props.name}</li>
+        <button onClick={this.handleClick}>DELETE</button>
       </div>
     );
   }
